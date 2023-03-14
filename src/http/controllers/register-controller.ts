c0e9ffa0852748014d089e8a9e04497f2f1a7645
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-03-13 22:28:00 -0300

    feat: verify email

--- src/http/controllers/register-controller.ts
+++ src/http/controllers/register-controller.ts
@@ -1,6 +1,7 @@
 import { prisma } from '@/lib/prisma'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
+import { hash } from 'bcryptjs'
 
 export async function register(req: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -11,11 +12,25 @@ export async function register(req: FastifyRequest, reply: FastifyReply) {
 
   const { email, name, password } = registerBodySchema.parse(req.body)
 
+  const password_hash = await hash(password, 6)
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (userWithSameEmail) {
+    return reply.status(409).send({
+      message: 'Email invalid',
+    })
+  }
+
   await prisma.user.create({
     data: {
       email,
       name,
-      password_hash: password,
+      password_hash,
     },
   })
 
