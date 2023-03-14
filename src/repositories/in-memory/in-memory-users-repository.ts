93b673dbde3996bf8e13039b113889500ae9835d
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-03-14 18:09:21 -0300

    feat: create in memory user

---
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -0,0 +1,25 @@
+import { Prisma, User } from '@prisma/client'
+import { UsersRepository } from '../users-repository'
+
+export class InMemoryUsersRepository implements UsersRepository {
+  public items: User[] = []
+
+  async create(data: Prisma.UserCreateInput): Promise<User> {
+    const user = {
+      created_at: new Date(),
+      email: data.email,
+      id: 'random-id',
+      name: data.name,
+      password_hash: data.password_hash,
+    }
+    this.items.push(user)
+    return user
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    const user = this.items.find((user) => user.email === email)
+
+    if (!user) return null
+    return user
+  }
+}
