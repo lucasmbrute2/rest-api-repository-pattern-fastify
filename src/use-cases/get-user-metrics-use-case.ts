bfe9d7d5a73acd16c3c8c90e2e080d5dd2e09b31
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-03-15 19:19:53 -0300

    feat: use case get metrics

---
+++ src/use-cases/get-user-metrics-use-case.ts
@@ -0,0 +1,23 @@
+import { CheckInsRepository } from '@/repositories/check-ins-repository'
+
+interface GetUserMetricsUseCaseRequest {
+  userId: string
+}
+
+interface GetUserMetricsUseCaseResponse {
+  checkinsCount: number
+}
+
+export class GetUserMetricsUseCase {
+  constructor(private checkInsRepository: CheckInsRepository) {}
+
+  async execute({
+    userId,
+  }: GetUserMetricsUseCaseRequest): Promise<GetUserMetricsUseCaseResponse> {
+    const checkinsCount = await this.checkInsRepository.countByUserId(userId)
+
+    return {
+      checkinsCount,
+    }
+  }
+}
