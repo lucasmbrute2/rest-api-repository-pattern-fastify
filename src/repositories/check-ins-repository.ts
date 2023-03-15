43ffc78056fe84375110cbd555dfc57e82b2a43e
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-03-15 16:35:31 -0300

    feat: findByUserIdOnDate method

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -3,4 +3,5 @@ import { CheckIn, Prisma } from '@prisma/client'
 export interface CheckInsRepository {
   // unchecked serve para pegar o ID de relações já criadas
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
+  findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
 }
