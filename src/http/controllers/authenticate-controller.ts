8019698afc87626ff964eea197423c2270bb23c0
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-03-15 01:25:34 -0300

    feat: implement authenticate factory into controller

--- src/http/controllers/authenticate-controller.ts
+++ src/http/controllers/authenticate-controller.ts
@@ -1,6 +1,5 @@
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
-import { AuthenticateUseCase } from '@/use-cases/authenticate'
 import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
+import { makeAuthenticateUseCase } from '@/use-cases/factories/make-authenticate-use-case'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 
@@ -13,8 +12,8 @@ export async function authenticate(req: FastifyRequest, reply: FastifyReply) {
   const { email, password } = authenticateBodySchema.parse(req.body)
 
   try {
-    const prismaUsersRepository = new PrismaUsersRepository()
-    await new AuthenticateUseCase(prismaUsersRepository).execute({
+    const prismaUsersRepository = makeAuthenticateUseCase()
+    await prismaUsersRepository.execute({
       email,
       password,
     })
