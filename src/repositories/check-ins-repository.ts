1dedf8c4ce9a5a22d1b8f709d56480da98f104c4
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-03-15 23:47:02 -0300

    feat: add findByID method

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -6,4 +6,6 @@ export interface CheckInsRepository {
   findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
   findManyByUserId(id: string, page: number): Promise<CheckIn[]>
   countByUserId(id: string): Promise<number>
+  findById(id: string): Promise<CheckIn | null>
+  save(checkIn: CheckIn): Promise<CheckIn>
 }
