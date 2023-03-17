e023c081d22b5ce546a9ed4a71e61fc28e9a2c81
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-03-17 19:51:39 -0300

    feat: gyms check in controller

--- src/http/controllers/gyms/search.ts
+++ src/http/controllers/gyms/search.ts
@@ -8,7 +8,7 @@ export async function search(req: FastifyRequest, reply: FastifyReply) {
     page: z.coerce.number().min(1).default(1),
   })
 
-  const { page, q } = searchGymsQuerySechema.parse(req.body)
+  const { page, q } = searchGymsQuerySechema.parse(req.query)
 
   const searchGymsUseCase = makeSearchGymsUseCase()
   const { gyms } = await searchGymsUseCase.execute({
