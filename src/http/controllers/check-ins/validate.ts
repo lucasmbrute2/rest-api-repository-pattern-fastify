d71559a131ac968a1db928e27f5e8dc4be8be508
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-03-17 19:51:24 -0300

    feat: route check in controller

---
+++ src/http/controllers/check-ins/validate.ts
@@ -0,0 +1,18 @@
+import { makeValidateCheckInUseCase } from '@/use-cases/factories/make-validate-check-in-use-case'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function validate(req: FastifyRequest, reply: FastifyReply) {
+  const validateCheckInParamsSchema = z.object({
+    checkInId: z.string().uuid(),
+  })
+
+  const { checkInId } = validateCheckInParamsSchema.parse(req.params)
+
+  const validateCheckinUseCase = makeValidateCheckInUseCase()
+  await validateCheckinUseCase.execute({
+    checkInId,
+  })
+
+  return reply.status(204).send()
+}
