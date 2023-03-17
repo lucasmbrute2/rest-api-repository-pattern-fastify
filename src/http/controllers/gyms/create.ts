93bf7a6f71d554b2d8d8ea585afa6d94a78b51a1
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-03-17 12:58:05 -0300

    feat: create gym controller

---
+++ src/http/controllers/gyms/create.ts
@@ -0,0 +1,27 @@
+import { makeCreateGymUseCase } from '@/use-cases/factories/make-create-gym-use-case'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function register(req: FastifyRequest, reply: FastifyReply) {
+  const createGymBodySchema = z.object({
+    title: z.string(),
+    description: z.string().nullable(),
+    phone: z.string().nullable(),
+    latitude: z.number().refine((value) => Math.abs(value) <= 90),
+    longitude: z.number().refine((value) => Math.abs(value) <= 180),
+  })
+
+  const { description, title, latitude, longitude, phone } =
+    createGymBodySchema.parse(req.body)
+
+  const registerUseCase = makeCreateGymUseCase()
+  await registerUseCase.execute({
+    description,
+    title,
+    latitude,
+    longitude,
+    phone,
+  })
+
+  return reply.status(201).send()
+}
