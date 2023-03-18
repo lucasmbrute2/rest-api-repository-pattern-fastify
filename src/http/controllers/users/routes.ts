c94ff57a60974a07634b29bc890c1ca00f7ece7e
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-03-17 21:13:55 -0300

    feat: route

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,11 +3,13 @@ import { authenticate } from './authenticate-controller'
 import { profile } from './profile-controller'
 import { register } from './register-controller'
 import { verifyJWT } from '../../middlewares/verify-jwt'
+import { refresh } from './refresh'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   // pensar nas rotas como entidades
   app.post('/sessions', authenticate)
+  app.patch('/token/refresh', refresh)
 
   //* Protected routes*/
   app.get('/me', { onRequest: [verifyJWT] }, profile)
